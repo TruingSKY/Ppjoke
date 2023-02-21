91d2db2cb663b35d6d672727518e08c11a73f76f
topsun <linuxunis@163.com>
2023-02-21 23:10:40 +0800

    commit

--- Ppjoke/app/src/test/java/com/example/ppjoke/ExampleUnitTest.java
+++ Ppjoke/app/src/test/java/com/example/ppjoke/ExampleUnitTest.java
@@ -1,8 +1,13 @@
 package com.example.ppjoke;
 
+import org.json.JSONObject;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import com.example.libnetwork.ApiResponse;
+import com.example.libnetwork.GetRequest;
+import com.example.libnetwork.JsonCallback;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +18,28 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void TestHttp() {
+        GetRequest<JSONObject> request = new GetRequest<JSONObject>("http://www.baidu.com");
+//        request.execute();
+
+        request.execute(new JsonCallback<JSONObject>() {
+            @Override
+            public void onSuccess(ApiResponse <JSONObject>response) {
+                super.onSuccess(response);
+            }
+
+            @Override
+            public void onError(ApiResponse<JSONObject> response) {
+                super.onError(response);
+            }
+
+            @Override
+            public void onCacheSuccess(ApiResponse<JSONObject> response) {
+                super.onCacheSuccess(response);
+            }
+        });
     }
 }
