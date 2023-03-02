d239435d3393a6484ffa1f70cf33a4862ed2038b
topsun <linuxunis@163.com>
2023-03-02 23:48:50 +0800

    圆角

--- Ppjoke/libcommon/src/main/java/com/example/libcommon/global/view/RoundFrameLayout.java
+++ Ppjoke/libcommon/src/main/java/com/example/libcommon/global/view/RoundFrameLayout.java
@@ -9,18 +9,21 @@ import androidx.annotation.Nullable;
 
 public class RoundFrameLayout extends FrameLayout {
     public RoundFrameLayout(@NonNull Context context) {
-        super(context);
+        this(context,null);
     }
 
     public RoundFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs,0);
     }
 
     public RoundFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        this(context, attrs, defStyleAttr,0);
     }
 
     public RoundFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+
+
+
     }
 }
